916d513b03881f52df151674bfd25b7171556b19
Ryang200 <231141506+Ryang200@users.noreply.github.com>
2025-10-17 17:59:39 +0000

    new

---
+++ Game.java
@@ -0,0 +1,44 @@
+public class Game {
+    private String gameName;
+    private int players;
+    private int score;
+
+    public Game(String gameName, int players) {
+        this.gameName = gameName;
+        this.players = players;
+        score = 0;
+    }
+
+    public String getGameName(){
+        return gameName;
+    }
+    
+    public int getPlayers(){
+        return players;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void addPlayer() {
+        players++;
+    }
+
+    public void increaseScore(int addedScore){
+        score += addedScore;
+    }
+
+    public double averageScorePerPlayer(){
+        return (double) score/players;
+    }
+
+    public boolean isGameOver(){
+        if ( score > 9 ) {
+            return true;
+            }
+        else {
+            return false;
+        }
+    }
+}
---
+++ U1T11/LabRunner.java
@@ -0,0 +1,11 @@
+public class LabRunner {
+    public static void main(String[] args){
+        System.out.println("Nice to see you!");
+System.out.println("Goodbye!");
+
+System.out.print("Nice to see you!\n");
+System.out.println("Goodbye!");
+
+
+    }
+}
